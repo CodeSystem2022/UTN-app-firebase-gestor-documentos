b9caa526599ee68916a00e9d2409f10ef9c2a515
Tamara Piccinni <tamarapiccinni@gmail.com>
2023-11-05 17:41:14 -0300

    Add more error handling for Firebase authentication

--- Firebase-React/src/utils/erroresFirebase.js
+++ Firebase-React/src/utils/erroresFirebase.js
@@ -1,14 +1,39 @@
+// erroresFirebase.js toma un código de error de Firebase como entrada y devuelve un objeto con un código 
+//y un mensaje de error específicos de la aplicación.
+
+// Definición de la función erroresFirebase que toma un código de error como entrada
 export const erroresFirebase = (code) => {
+    // Utiliza un switch para determinar qué tipo de error de Firebase se ha producido
     switch (code) {
-        case 'auth/invalid-email':
-            return 'El correo electrónico no es válido.'; // Mensaje de error para correo electrónico inválido.
-        case 'auth/user-disabled':
-            return 'El usuario está deshabilitado.'; // Mensaje de error para usuario deshabilitado.
-        case 'auth/user-not-found':
-            return 'El usuario no existe.'; // Mensaje de error para usuario no encontrado.
-        case 'auth/wrong-password':
-            return 'La contraseña no es válida.'; // Mensaje de error para contraseña incorrecta.
+        case "auth/email-already-in-use":
+            // Si el código es "auth/email-already-in-use", devuelve un objeto con un código y mensaje específicos
+            return {
+                code: "email",
+                message: "Usuario ya registrado",
+            };
+        case "auth/invalid-email":
+            // Si el código es "auth/invalid-email", devuelve un objeto con un código y mensaje específicos
+            return {
+                code: "email",
+                message: "Formato email no válido",
+            };
+        case "auth/user-not-found":
+            // Si el código es "auth/user-not-found", devuelve un objeto con un código y mensaje específicos
+            return {
+                code: "email",
+                message: "Usuario no registrado",
+            };
+        case "auth/wrong-password":
+            // Si el código es "auth/wrong-password", devuelve un objeto con un código y mensaje específicos
+            return {
+                code: "password",
+                message: "Contraseña incorrecta",
+            };
         default:
-            return 'Error del servidor, intente más tarde.'; // Mensaje de error por defecto en caso de otros errores.
+            // Si el código no coincide con ninguno de los casos anteriores, devuelve un objeto con un código y mensaje genéricos
+            return {
+                code: "email",
+                message: "Ocurrió un error en el servidor",
+            };
     }
-}
+};
